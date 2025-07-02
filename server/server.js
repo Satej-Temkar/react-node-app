080517f87092bef273c09eb9ca15b987e76c50ff
Babaji Temkar <126382725+Satej-Temkar@users.noreply.github.com>
2025-07-02 16:52:38 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from 'cors';
+import path from 'path';
 import userRoutes from './routes/userRoutes.js'
 
 const app = express();
@@ -7,7 +8,14 @@ const PORT = 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, "client", "build")));
 
 app.use("/api/user", userRoutes);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(PORT, () => {console.log(`server running on port ${PORT}`)})
