6d2f57424445848f0467aad64390641a6ab01f3d
Babaji Temkar <126382725+Satej-Temkar@users.noreply.github.com>
2025-07-02 15:39:14 +0530

    Update DisplayPage.jsx

--- client/src/pages/DisplayPage.jsx
+++ client/src/pages/DisplayPage.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import dayjs from "dayjs";
 import { useNavigate } from "react-router-dom";
-const API_URL = "https://react-node-app-server.onrender.com";
+const API_URL = "https://react-node-app-ser.onrender.com";
 
 const DisplayPage = () => {
   const [user, setUser] = useState(null);
